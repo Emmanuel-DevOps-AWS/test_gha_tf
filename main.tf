c5260d32e4ff9fec1e4e0a921118b6a3dbf28a84
Emma A <emmanuel.abolar@gmail.com>
2024-09-17 13:53:56 -0400

    fix

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-resource "aws_s3_bucket" "name" {
-  bucket   =           "eleuecbidi"
+resource "aws_instance" "example"{
+ami="ami-0c55b159cbfafe1f0"
+instance_type="t2.micro"
+tags ={
+Name="ExampleInstance"
+Environment = "Dev"
+}
+security_groups = ["sg-12345","sg-67890"]
 }
--- main5.tf
+++ main5.tf
@@ -1,4 +1,4 @@
 resource "aws_ec2_instance_state" "name" {
-  instance_id     =      "Dvqeinrd"
-    state       =     "up"
+  instance_id = "Dvqeinrd"
+  state       = "up"
 }
